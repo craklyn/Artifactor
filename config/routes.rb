44d4632d2bb1055c7820fc95550b0b3f95b77d64
Daniel Blackburn <craklyn@gmail.com>
2016-11-19 17:06:59 -0800

    Doing something with artifacts....?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'students#index', as: :students
+  get '/students/:id/artifacts', to: 'artifacts#index', as: :student_artifacts
 
   root "students#index"
 end
